a6d34f7ab9fcf7b6da7493cc0059b83b01777cfa
Georgios Karseras <p2424630@my365.dmu.ac.uk>
2020-11-15 13:12:23 +0000

    Fixed setup

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 # @Date:   6 Nov 2020 00:33
 
 from setuptools import setup, find_packages
-from import __version__
 
 setup(
     name='propcalc',
-    version=__version__,
     extras_require=dict(test=['pytest']),
     packages=find_packages(where='src'),
     package_dir={"": "src"}
